1cbc908728f3193a865a7f91bc2fb996970d3e09
mentierd <dayne.mentier@gmail.com>
2020-11-26 11:56:09 -0500

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,70 @@
+module.exports = (app) => {
+    const baseConfig = {
+        presets: ['@babel/preset-react', '@babel/typescript'],
+        plugins: [
+            'babel-plugin-macros',
+            ['babel-plugin-set-display-name', {setName: true}],
+            '@babel/plugin-transform-react-constant-elements',
+            [
+                '@babel/plugin-proposal-decorators',
+                {
+                    legacy: true
+                }
+            ],
+            [
+                '@babel/plugin-proposal-class-properties',
+                {
+                    loose: true
+                }
+            ],
+            [
+                '@babel/plugin-transform-typescript',
+                {
+                    isTSX: true
+                }
+            ],
+            [
+                'babel-plugin-styled-components',
+                {
+                    minify: true,
+                    pure: true,
+                    ssr: false,
+                    transpileTemplateLiterals: false
+                }
+            ],
+            'babel-plugin-lodash',
+            '@babel/plugin-transform-runtime'
+        ]
+    };
+
+    return {
+        presets: [
+            ...baseConfig.presets,
+            ...(app.env('test')
+                ? [
+                      [
+                          '@babel/preset-env',
+                          {
+                              targets: {
+                                  node: 'current'
+                              }
+                          }
+                      ]
+                  ]
+                : [
+                      [
+                          '@babel/preset-env',
+                          {
+                              targets: {
+                                  browsers: ['last 2 versions']
+                              },
+                              corejs: 3,
+                              useBuiltIns: 'usage',
+                              ...(process.env.IS_ESM ? {modules: false} : {})
+                          }
+                      ]
+                  ])
+        ],
+        plugins: baseConfig.plugins
+    };
+};
